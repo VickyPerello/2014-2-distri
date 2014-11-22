38f34410e750eb8d6eb4ed0dca14d12056442cec
Vicky <victoriajperello@gmail.com>
2014-11-22 11:45:50 -0300

    entrega final

--- scripts/create_taps.sh
+++ scripts/create_taps.sh
@@ -17,15 +17,11 @@ sleep 1
 #Servers
 openvpn --remote $1 --port 1983 --dev tap0 --ifconfig 245.2.18.82 255.255.255.0 10.8.11.1 &
 openvpn --remote $1 --port 1984 --dev tap1 --ifconfig 245.2.18.82 255.255.255.0 192.168.45.1 &
-openvpn --remote $1 --port 1985 --dev tap2 --ifconfig 245.2.18.82 255.255.255.0 10.51.111.130 &
-openvpn --remote $1 --port 1986 --dev tap3 --ifconfig 245.2.18.82 255.255.255.0 10.51.111.130 &
+openvpn --remote $1 --port 1985 --dev tap3 --ifconfig 245.2.18.82 255.255.255.0 10.51.111.130 &
+openvpn --remote $1 --port 1986 --dev tap2 --ifconfig 245.2.18.82 255.255.255.0 10.8.207.129 &
 
 #Hosts
 openvpn --remote $2 --port 2001 --dev tap11 --ifconfig 245.2.18.82 255.255.255.0 10.79.7.200 &
 openvpn --remote $2 --port 2002 --dev tap12 --ifconfig 245.2.18.82 255.255.255.0 10.51.111.200 &
 openvpn --remote $2 --port 2003 --dev tap13 --ifconfig 245.2.18.82 255.255.255.0 10.8.207.120 &
-
-
-
-#TODO: cuando este hecho, desde aca levantamos los servers
 
--- scripts/server/create_ftpserver.sh
+++ scripts/server/create_ftpserver.sh
@@ -15,33 +15,33 @@ openvpn --rmtun --dev tap13
 openvpn --remote $1 --port 1983 --dev tap0 --ifconfig 10.8.11.1 255.255.255.0 245.2.18.82 &
 sleep 25
 
-route add -net 10.8.207.0 netmask 255.255.255.224 gw 10.79.7.5 metric 1
-route add -net 192.168.45.0 netmask 255.255.255.0 gw 10.79.7.5 metric 1
-route add -net 10.8.207.32 netmask 255.255.255.224 gw 10.79.7.5 metric 1
-route add -net 10.51.111.0 netmask 255.255.255.0 gw 10.79.7.5 metric 1
-route add -net 10.8.207.128 netmask 255.255.255.128 gw 10.79.7.5 metric 1
-route add -net 10.8.210.0 netmask 255.255.255.224 gw 10.79.7.5 metric 1
-route add -net 10.8.210.32 netmask 255.255.255.224 gw 10.79.7.5 metric 1
-route add -net 10.8.207.64 netmask 255.255.255.192 gw 10.79.7.5 metric 1
-route add -net 10.8.11.0 netmask 255.255.255.0 gw 10.79.7.5 metric 1
-route add -net 10.8.210.68 netmask 255.255.255.252 gw 10.79.7.5 metric 1
-route add -net 10.8.210.72 netmask 255.255.255.252 gw 10.79.7.5 metric 1
-route add -net 10.8.210.76 netmask 255.255.255.252 gw 10.79.7.5 metric 1
-route add -net 10.8.210.80 netmask 255.255.255.252 gw 10.79.7.5 metric 1
-route add -net 10.8.210.84 netmask 255.255.255.252 gw 10.79.7.5 metric 1
-route add -net 10.8.210.88 netmask 255.255.255.252 gw 10.79.7.5 metric 1
-route add -net 10.8.210.92 netmask 255.255.255.252 gw 10.79.7.5 metric 1
-route add -net 10.8.210.64 netmask 255.255.255.252 gw 10.79.7.5 metric 1
-route add -net 172.19.3.64 netmask 255.255.255.252 gw 10.79.7.5 metric 1
-route add -net 172.19.3.68 netmask 255.255.255.252 gw 10.79.7.5 metric 1
-route add -net 172.19.3.72 netmask 255.255.255.252 gw 10.79.7.5 metric 1
-route add -net 172.19.3.76 netmask 255.255.255.252 gw 10.79.7.5 metric 1
-route add -net 172.19.3.80 netmask 255.255.255.252 gw 10.79.7.5 metric 1
-route add -net 172.19.3.84 netmask 255.255.255.252 gw 10.79.7.5 metric 1
-route add -net 172.19.3.88 netmask 255.255.255.252 gw 10.79.7.5 metric 1
-route add -net 172.19.3.92 netmask 255.255.255.252 gw 10.79.7.5 metric 1
-route add -net 172.19.3.96 netmask 255.255.255.252 gw 10.79.7.5 metric 1
-route add -net 172.19.3.100 netmask 255.255.255.252 gw 10.79.7.5 metric 1
+route add -net 10.8.207.0 netmask 255.255.255.224 gw 10.8.11.6 metric 1
+route add -net 192.168.45.0 netmask 255.255.255.0 gw 10.8.11.6 metric 1
+route add -net 10.79.7.0 netmask 255.255.255.0 gw 10.8.11.6 metric 1
+route add -net 10.8.207.32 netmask 255.255.255.224 gw 10.8.11.6 metric 1
+route add -net 10.51.111.0 netmask 255.255.255.0 gw 10.8.11.6 metric 1
+route add -net 10.8.207.128 netmask 255.255.255.128 gw 10.8.11.6 metric 1
+route add -net 10.8.210.0 netmask 255.255.255.224 gw 10.8.11.6 metric 1
+route add -net 10.8.210.32 netmask 255.255.255.224 gw 10.8.11.6 metric 1
+route add -net 10.8.207.64 netmask 255.255.255.192 gw 10.8.11.6 metric 1
+route add -net 10.8.210.68 netmask 255.255.255.252 gw 10.8.11.16 metric 1
+route add -net 10.8.210.72 netmask 255.255.255.252 gw 10.8.11.16 metric 1
+route add -net 10.8.210.76 netmask 255.255.255.252 gw 10.8.11.16 metric 1
+route add -net 10.8.210.80 netmask 255.255.255.252 gw 10.8.11.16 metric 1
+route add -net 10.8.210.84 netmask 255.255.255.252 gw 10.8.11.16 metric 1
+route add -net 10.8.210.88 netmask 255.255.255.252 gw 10.8.11.16 metric 1
+route add -net 10.8.210.92 netmask 255.255.255.252 gw 10.8.11.16 metric 1
+route add -net 10.8.210.64 netmask 255.255.255.252 gw 10.8.11.6 metric 1
+route add -net 172.19.3.64 netmask 255.255.255.252 gw 10.8.11.6 metric 1
+route add -net 172.19.3.68 netmask 255.255.255.252 gw 10.8.11.6 metric 1
+route add -net 172.19.3.72 netmask 255.255.255.252 gw 10.8.11.6 metric 1
+route add -net 172.19.3.76 netmask 255.255.255.252 gw 10.8.11.6 metric 1
+route add -net 172.19.3.80 netmask 255.255.255.252 gw 10.8.11.6 metric 1
+route add -net 172.19.3.84 netmask 255.255.255.252 gw 10.8.11.6 metric 1
+route add -net 172.19.3.88 netmask 255.255.255.252 gw 10.8.11.6 metric 1
+route add -net 172.19.3.92 netmask 255.255.255.252 gw 10.8.11.6 metric 1
+route add -net 172.19.3.96 netmask 255.255.255.252 gw 10.8.11.6 metric 1
+route add -net 172.19.3.100 netmask 255.255.255.252 gw 10.8.11.6 metric 1
 
 
 cp vsftpd.conf /etc/
